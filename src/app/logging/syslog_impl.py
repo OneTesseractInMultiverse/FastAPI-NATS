76f00156ad89d8087f2cb279b1f8158ce67b4b8a
Pedro Guzmán <pedro.andres.guzman@ibm.com>
2021-03-04 18:02:37 -0600

    Abstractions and implementations for basic logging and implementation of NATS client

--- src/app/logging/syslog_impl.py
+++ src/app/logging/syslog_impl.py
@@ -47,23 +47,35 @@ class LogEvent(object):
 # -----------------------------------------------------------------------------
 # CLASS SIMPLE LOGGER
 # -----------------------------------------------------------------------------
-class SimpleAppSecLogger(MicroserviceLogger):
+class SimpleLogger(MicroserviceLogger):
 
+    # -------------------------------------------------------------------------
+    # METHOD DEBUG
+    # -------------------------------------------------------------------------
     def debug(self, message: str, **kwargs) -> dict:
         event = LogEvent(message=message, level=LogLevel.DEBUG)
         logging.debug(str(event))
         return event.dict()
 
+    # -------------------------------------------------------------------------
+    # METHOD INFO
+    # -------------------------------------------------------------------------
     def info(self, message: str, **kwargs) -> dict:
         event = LogEvent(message=message, level=LogLevel.INFO)
         logging.info(str(event))
         return event.dict()
 
+    # -------------------------------------------------------------------------
+    # METHOD WARNING
+    # -------------------------------------------------------------------------
     def warning(self, message: str, **kwargs) -> dict:
         event = LogEvent(message=message, level=LogLevel.WARNING)
         logging.warning(str(event))
         return event.dict()
 
+    # -------------------------------------------------------------------------
+    # METHOD ERROR
+    # -------------------------------------------------------------------------
     def error(self, message: str, **kwargs) -> dict:
         event = LogEvent(message=message, level=LogLevel.WARNING)
         logging.warning(str(event))
